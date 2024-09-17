79840a928a0039e2966eb5ee98d2c21d9c888f8c
SudoAldebaran <rayanmagniac@gmail.com>
2024-09-17 18:30:30 +0200

    README UPDATE

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,11 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer style={{
-      backgroundColor: '#333', // doc FOOTER BACKGROUND COLOR
-      color: '#fff',           // doc FOOTER TEXT COLOR
-      textAlign: 'center',     // doc CENTER TEXT ALIGNMENT
-      padding: '10px 0',       // doc TOP AND BOTTOM PADDING
-      width: '100%',           // doc FULL WIDTH FOOTER
+      backgroundColor: '#333', 
+      color: '#fff',           
+      textAlign: 'center',     
+      padding: '10px 0',       
+      width: '100%',           
       marginTop: 'auto',       // doc PUSH FOOTER TO BOTTOM IN FLEX CONTAINER
     }}>
       <p>© 2024 Canine Vista. All rights reserved.</p> {/* doc COPYRIGHT TEXT */}
